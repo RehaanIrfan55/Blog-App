121b6f767e3eabc1781ff79dea01a1f1a439f1aa
Rehaan12355 <rehaan_irfan@outlook.com>
2025-08-05 04:25:54 -0700

    Added user model

--- database.py
+++ database.py
@@ -2,9 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Change these to match your DB
-DATABASE_URL = "postgresql://blog_user:blogpass@localhost/fastapi_blog"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine("postgresql://postgres:secretpassword@localhost:5432/blogging_app")
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
---
+++ main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+from models import Base
+from database import engine
+
+Base.metadata.create_all(bind=engine)
+app = FastAPI()
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
